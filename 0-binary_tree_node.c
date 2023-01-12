cbabb4fddb0358fdcfac8f8cd0af8c6125dcb00a
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-01-12 10:11:25 -0800

    Task 00

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,6 +1,10 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_t - Is a function that creates a binary tree node
+ * @parent: father node
+ * @value: element of the node
  *
+ * Return: new node, or NULL if it failed
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
