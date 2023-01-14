180a6db91ae0851d04f70e3bdf37f1b68b6d2b86
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-01-14 10:58:11 -0800

    Task 08

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 /**
- * binary_tree_postorder: is a function that goes through a
- * binary tree using in-order traversal.
+ * binary_tree_postorder - Is a function that goes through
+ * a binary tree using in-order traversal.
  * @tree: is a pointer to the root node of the tree to traverse
  * @func: is a pointer to a function to call for each node
  */
